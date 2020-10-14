8b32a17913a15d7ac3db2d5dd0af2228542e6394
Bastien Dewerse <bastien.dewerse@gmail.com>
2020-10-14 14:17:51 +0200

    burger menu made accessible

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -9,11 +9,11 @@ const Header = () => {
             <nav class="navbar bg-transparent" role="navigation" aria-label="main navigation">
                 <div class="navbar-brand">
                     <Link to="/" class={`navbar-item nav-link${useLocation().pathname === "/" ? " is-current" : ""}`}>Woodstock</Link>
-                    <a role="button" class={`navbar-burger burger${isBurgerActive ? " is-active" : ""}`} aria-label="menu" aria-expanded="false" data-target="mainMenu" onClick={() => setisBurgerActive(!isBurgerActive)}>
+                    <button role="button" class={`navbar-burger burger${isBurgerActive ? " is-active" : ""}`} aria-label="menu" aria-expanded="false" data-target="mainMenu" onClick={() => setisBurgerActive(!isBurgerActive)}>
                         <span aria-hidden="true"></span>
                         <span aria-hidden="true"></span>
                         <span aria-hidden="true"></span>
-                    </a>
+                    </button>
                 </div>
                 <div id="mainMenu" class={`navbar-menu bg-transparent${isBurgerActive ? " is-active" : ""}`}>
                     <div class="navbar-end">
