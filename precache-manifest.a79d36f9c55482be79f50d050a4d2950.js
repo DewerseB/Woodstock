bb977d1991863a2301a17da4370f627170f3fd94
Bastien Dewerse <bastien.dewerse@gmail.com>
2020-10-14 16:04:30 +0200

    Updates

---
+++ precache-manifest.a79d36f9c55482be79f50d050a4d2950.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "745401f84574e131c6513a1ff023c662",
+    "url": "/Woodstock/index.html"
+  },
+  {
+    "revision": "99209348d3e4125e1cbc",
+    "url": "/Woodstock/static/css/2.98705a83.chunk.css"
+  },
+  {
+    "revision": "1e308f3bdc481774597f",
+    "url": "/Woodstock/static/css/main.372c1eae.chunk.css"
+  },
+  {
+    "revision": "99209348d3e4125e1cbc",
+    "url": "/Woodstock/static/js/2.9cc0dea6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Woodstock/static/js/2.9cc0dea6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e308f3bdc481774597f",
+    "url": "/Woodstock/static/js/main.0af4a6bc.chunk.js"
+  },
+  {
+    "revision": "f0008e81d330ec5d9b10",
+    "url": "/Woodstock/static/js/runtime-main.4d8c00a6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Woodstock/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2b3a13954fb976800801cdcc946c7097",
+    "url": "/Woodstock/static/media/wood-stack.2b3a1395.jpg"
+  }
+]);
