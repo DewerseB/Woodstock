2ed02caae197e2cd3118f17c1e545a129d9b0c6a
Bastien Dewerse <bastien.dewerse@gmail.com>
2020-10-27 16:59:02 +0100

    Form validation client side

--- src/components/contact-form/contact-form.jsx
+++ src/components/contact-form/contact-form.jsx
@@ -2,10 +2,33 @@ import React, { useState, useEffect } from 'react';
 import './contact-form.css';
 
 const ContactForm = () => {
-    const [validFirstname, setValidFirstname] = useState(true);
+    const [firstname, setFirstname] = useState("");
+    const [lastname, setLastname] = useState("");
+    const [email, setEmail] = useState("");
+    const mailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
     useEffect(() => {
-        
+        if (firstname.length > 1) {
+            document.getElementById('form-firstname').parentNode.querySelector('span').classList.add("has-text-success");
+        } else {
+            document.getElementById('form-firstname').parentNode.querySelector('span').classList.remove("has-text-success");
+        }
+        if (lastname.length > 1) {
+            document.getElementById('form-lastname').parentNode.querySelector('span').classList.add("has-text-success");
+        } else {
+            document.getElementById('form-lastname').parentNode.querySelector('span').classList.remove("has-text-success");
+        }
+        if (mailRegex.test(email)) {
+            document.getElementById('email').parentNode.querySelectorAll('span')[0].classList.add("has-text-success");
+            document.getElementById('email').parentNode.querySelectorAll('span')[1].classList.add("has-text-success");
+            document.getElementById('email').parentNode.querySelectorAll('span')[0].classList.remove("has-text-danger");
+            document.getElementById('email').parentNode.querySelectorAll('span')[1].classList.remove("has-text-danger");
+        } else if (email.length > 0) {
+            document.getElementById('email').parentNode.querySelectorAll('span')[0].classList.remove("has-text-success");
+            document.getElementById('email').parentNode.querySelectorAll('span')[1].classList.remove("has-text-success");
+            document.getElementById('email').parentNode.querySelectorAll('span')[0].classList.add("has-text-danger");
+            document.getElementById('email').parentNode.querySelectorAll('span')[1].classList.add("has-text-danger");
+        }
     });
 
     return (
@@ -13,30 +36,37 @@ const ContactForm = () => {
             <h2 className="title is-2">Contact form</h2>
             <form className="container box">
                 <div className="field is-grouped is-grouped-centered">
-                    <div className="control is-expanded">
+                    <div className="container">
                         <label className="label" htmlFor="form-firstname">First name</label>
-                        <input id="form-firstname" className="input" type="text" placeholder="First name" required/>
+                        <div className="control is-expanded has-icons-right">
+                            <input id="form-firstname" className="input" type="text" placeholder="First name" onChange={(e) => setFirstname(e.target.value)} required/>
+                            <span className="icon is-small is-right">
+                                <i className="fas fa-check"></i>
+                            </span>
+                        </div>
                     </div>
-                    <div className="control is-expanded has-icons-right">
+                    <div className="container">
                         <label className="label" htmlFor="form-lastname">Last name</label>
-                        <input id="form-lastname" className="input" type="text" placeholder="Last name" required/>
-                        {/* <span class="icon is-small is-right">
-                            <i class="fas fa-check"></i>
-                        </span> */}
+                        <div className="control is-expanded has-icons-right">
+                            <input id="form-lastname" className="input" type="text" placeholder="Last name" onChange={(e) => setLastname(e.target.value)} required/>
+                            <span className="icon is-small is-right">
+                                <i className="fas fa-check"></i>
+                            </span>
+                        </div>
                     </div>
                 </div>
                 <div className="field">
                     <label className="label" htmlFor="email">Email</label>
                     <div className="control has-icons-left has-icons-right">
-                        <input id="email" className="input" type="email" placeholder="Email" required/>
+                        <input id="email" className="input" type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} required/>
                         <span className="icon is-small is-left">
-                        <i className="fas fa-envelope"></i>
+                            <i className="fas fa-envelope"></i>
                         </span>
                         <span className="icon is-small is-right">
-                        <i className="fas fa-exclamation-triangle"></i>
+                            <i className="fas fa-exclamation-triangle"></i>
                         </span>
                     </div>
-                    <p className="help is-danger">This email is invalid</p>
+                    <p className="help is-danger"></p>
                 </div>
                 <div className="field">
                     <label className="label" htmlFor="form-message">Message</label>
